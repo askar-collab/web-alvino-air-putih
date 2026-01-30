8a9565046462e70485e4a596c9872e4c4ef8afd4
askar-collab <tkj151181@gmail.com>
2026-01-30 08:53:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function pesanSekarang() {
+    alert("Terima kasih sudah memilih Alvino Air Putih! 💧");
+}document.addEventListener("DOMContentLoaded", function () {
+    const tombol = document.getElementById("pesanBtn");
+
+    tombol.addEventListener("click", function () {
+        alert("Terima kasih sudah memesan Alvino Air Putih! 💧");
+    });
+});
